626b9223cb58cb7853937e2ce8a666c49a6b1050
Arthur Novello <arthurnovello@users.noreply.github.com>
2017-03-18 18:10:10 -0300

    Add files via upload

---
+++ PYTHON/Ex09.py
@@ -0,0 +1,7 @@
+nome = input()
+fsl = float(input())
+vnd = float(input())
+
+sal = fsl + (0.15*vnd)
+
+print("TOTAL = R$ %.2f" % (sal))
---
+++ PYTHON/Ex10.py
@@ -0,0 +1,7 @@
+A, B, C =  map(float, input().split())
+D, E, F =  map(float, input().split())
+
+vlp = (B*C)+(F*E)
+
+print("VALOR A PAGAR: R$ %.2f" % (vlp))
+
