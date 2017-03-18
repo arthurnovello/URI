3cdea11682f5eb634ba34b2d309a117721e84390
Arthur Novello <arthurnovello@users.noreply.github.com>
2017-03-18 16:16:38 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package problema1001;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n1 = scn.nextInt();
+		int n2 = scn.nextInt();
+		System.out.println("X = "+ (n1+n2));
+		scn.close();
+	}
+
+}
