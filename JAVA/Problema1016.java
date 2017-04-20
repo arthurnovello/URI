628223b131c409f63bb7e1e0ed33d986c5f856fd
Arthur Novello <arthurnovello@users.noreply.github.com>
2017-04-20 10:38:49 -0300

    Create Problema1016.java

---
+++ JAVA/Problema1016.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+		Scanner scn = new Scanner(System.in);
+		int dis = scn.nextInt();
+		int tempo = dis * 2;
+		
+		System.out.println(tempo + " minutos");
+ 
+    }
+ 
+}
