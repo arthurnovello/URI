840cc18c2bedae8fbfb1583086dff3f2e5aa4c10
Arthur Novello <arthurnovello@users.noreply.github.com>
2017-04-26 23:57:29 -0300

    Create Problema1050.java

---
+++ JAVA/Problema1050.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		
+		int X = scn.nextInt();
+
+		
+		switch(X){
+		case 61:
+			System.out.println("Brasilia");
+			break;
+		case 71:
+			System.out.println("Salvador");
+			break;
+		case 11:
+			System.out.println("Sao Paulo");
+			break;
+		case 21:
+			System.out.println("Rio de Janeiro");
+			break;
+		case 32:
+			System.out.println("Juiz de Fora");
+			break;
+		case 19:
+			System.out.println("Campinas");
+			break;
+		case 27:
+			System.out.println("Vitoria");
+			break;
+		case 31:
+			System.out.println("Belo Horizonte");
+			break;
+		default:
+			System.out.println("DDD nao cadastrado");
+			break;
+		}
+		
+		scn.close();
+	}
+
+}
