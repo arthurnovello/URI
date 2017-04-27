70289e09a172d38d4f2d30f3736b766ee6bf502e
Arthur Novello <arthurnovello@users.noreply.github.com>
2017-04-26 22:24:01 -0300

    Create Problema1037.java

---
+++ JAVA/Problema1037.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		
+		float N = scn.nextFloat();
+		
+		if(N>=0 && N<=25){
+			System.out.println("Intervalo [0,25]");
+			
+		}else if(N>25 && N<=50){
+			System.out.println("Intervalo (25,50]");
+			
+		}else if(N>50 && N<=75){
+			System.out.println("Intervalo (50,75]");
+			
+		}else if(N>75 && N<=100){
+			System.out.println("Intervalo (75,100]");
+			
+		}else{
+			System.out.println("Fora de intervalo");
+			
+		}
+		
+		scn.close();
+	}
+
+}
