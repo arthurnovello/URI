1e56694ec9b31a84bcd18fc4cc7b978f45ecb64f
Arthur Novello <arthurnovello@users.noreply.github.com>
2017-05-08 23:15:46 -0300

    Create Problema1078.java

---
+++ JAVA/Problema1078.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		System.out.println("1 x "+n+" = "+(n*1));
+		System.out.println("2 x "+n+" = "+(n*2));
+		System.out.println("3 x "+n+" = "+(n*3));
+		System.out.println("4 x "+n+" = "+(n*4));
+		System.out.println("5 x "+n+" = "+(n*5));
+		System.out.println("6 x "+n+" = "+(n*6));
+		System.out.println("7 x "+n+" = "+(n*7));
+		System.out.println("8 x "+n+" = "+(n*8));
+		System.out.println("9 x "+n+" = "+(n*9));
+		System.out.println("10 x "+n+" = "+(n*10));
+		scn.close();
+	}
+
+}
