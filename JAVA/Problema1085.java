06974489425239598bc37918d4754ae7999cdb7a
Arthur Novello <arthurnovello@users.noreply.github.com>
2017-05-08 23:20:53 -0300

    Create Problema1085.java

---
+++ JAVA/Problema1085.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		for(int j=60; j>=0; j = j-5){
+			System.out.println("I="+i+" J="+j);
+			i=i+3;
+		}
+	}
+
+}
