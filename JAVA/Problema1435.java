982583f0d04ae7b4661c8a79225095bb4a9c4868
Arthur Novello <arthurnovello@users.noreply.github.com>
2017-06-20 17:25:54 -0300

    Create Problema1435.java

---
+++ JAVA/Problema1435.java
@@ -0,0 +1,54 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+  
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        BufferedReader br = new BufferedReader( new InputStreamReader(System.in)); 
+        int matriz[][] = new int[100][100];
+         
+        func(br, matriz); 
+    }
+     
+     static void func(BufferedReader br, int matriz[][]) throws IOException{
+        int tam = Integer.parseInt(br.readLine());
+        int cont = 1;
+        int ini = 0;
+        int fim = tam - 1;
+         
+        for(int j = 0; j < (tam+1) / 2; j++){
+            for(int i = ini; i <= fim; i++){
+                matriz[i][ini] = cont;
+                matriz[ini][i] = cont;
+                matriz[i][fim] = cont;
+                matriz[fim][i] = cont;
+            }
+            cont++;
+            ini++;
+            fim--;
+        }
+         
+        if(tam != 0){
+            for(int i = 0; i < tam; i++)
+                for(int j = 0; j < tam; j++)
+                    if ( j == tam - 1 )    
+                        System.out.printf("%3d\n", matriz[i][j]);
+                    else
+                        System.out.printf("%3d ", matriz[i][j]);
+             
+            System.out.printf("\n");
+             
+            func(br,matriz);
+        }
+ 
+    }
+ 
+}
